cc1c77918c3cc1dd9fd67cf49bf06bb3d910f85b
Marloes Kroonen <mkroonen@ilionx.com>
2018-12-24 15:33:30 +0100

    "Completed the module"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 @Component({
   selector: 'app-root',
